5f72d526ae87c16fc558309dfa1c479aaf2d8c45
Sudarshan <sudarshan97@gmail.com>
2017-04-15 14:19:05 +0530

    new

---
+++ app/src/main/java/com/example/hp/main/College.java
@@ -0,0 +1,73 @@
+package com.example.sudarshan.collegeenter;
+
+import static com.example.sudarshan.collegeenter.R.id.rank;
+
+/**
+ * Created by Sudarshan on 15-04-2017.
+ */
+
+public class College {
+    //int rank
+    String name,city,state,url,entrance;
+    double cutoff;
+    public  College ()
+    {
+
+    }
+
+//    public int getRank() {
+//        return rank;
+//    }
+//
+//    public void setRank(int rank) {
+//        this.rank = rank;
+//    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getEntrance() {
+        return entrance;
+    }
+
+    public void setEntrance(String entrance) {
+        this.entrance = entrance;
+    }
+
+    public double getCutoff() {
+        return cutoff;
+    }
+
+    public void setCutoff(double cutoff) {
+        this.cutoff = cutoff;
+    }
+}
